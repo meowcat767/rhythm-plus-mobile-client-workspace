af3eea4e172e267f26a2278772aea651df94636d
Splamei <instantveemo@gmail.com>
2026-02-22 18:08:57 +0000

    Updated the client to no longer use a deprecated function

--- app/src/main/java/com/splamei/rplus/client/MainActivity.java
+++ app/src/main/java/com/splamei/rplus/client/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -183,8 +184,9 @@ public class MainActivity extends AppCompatActivity
         webViewClient = new WebViewClient()
         {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
             {
+                String url = request.getUrl().toString();
                 if ((url.startsWith(mainUrl) && !url.contains(urlForNewTab)) || url.startsWith(urlToLoad))
                 {
                     // load my page
