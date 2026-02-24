a3a79cc698e09ce3a1706eb50f62afd76b1b46bb
Splamei <instantveemo@gmail.com>
2026-02-24 19:59:48 +0000

    Removed the dialog box for battery warnings

--- app/src/main/java/com/splamei/rplus/client/ui/error/BatteryWarn.java
+++ app/src/main/java/com/splamei/rplus/client/ui/error/BatteryWarn.java
@@ -23,13 +23,6 @@ public class BatteryWarn {
             if (batteryPct <= 20) {
                 // Toast warning
                 Toast.makeText(context, "Battery low! Please charge your device.", Toast.LENGTH_LONG).show();
-
-                // Dialog warning
-                new MaterialAlertDialogBuilder(context)
-                        .setTitle("Low Battery")
-                        .setMessage("Your battery is below 20%. It's recommended to charge your device.")
-                        .setPositiveButton("OK", null)
-                        .show();
             }
         }
     }
