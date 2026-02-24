71b2b52828d81a502c6d181f0185cc88a311babf
Splamei <instantveemo@gmail.com>
2026-02-24 12:44:06 +0000

    Removed an unused import

--- app/src/main/java/com/splamei/rplus/client/MainActivity.java
+++ app/src/main/java/com/splamei/rplus/client/MainActivity.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.View;
