e05547928714aea1eab49fe836588c6269209861
Ben <meowcat@meowcat.site>
2026-02-26 11:12:52 +0000

    add devtoast to isDevMode

--- app/src/main/java/com/splamei/rplus/client/checks/isDevMode.java
+++ app/src/main/java/com/splamei/rplus/client/checks/isDevMode.java
@@ -4,6 +4,8 @@ import static com.splamei.rplus.client.ui.settings.SettingsMenu.KEY_DEV_MODE;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.preference.PreferenceManager;
 
@@ -11,5 +13,12 @@ public class isDevMode {
     private static boolean isDevMode(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getBoolean(KEY_DEV_MODE, false);
+    }
+
+    public static void devToast(Context context, String message) {
+        if (isDevMode(context)) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            Log.d("DEV_MODE", message);
+        }
     }
 }
